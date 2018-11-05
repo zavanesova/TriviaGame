64f92672c9b30e67fef7c3882806f6d29c2640ef
z1995 <zhannaavanesova@yahoo.com>
2018-11-04 21:32:16 -0700

    Final Changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@ var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unanswered = 0;
 
+//timer - working correctly
 var minutesRemaining = 1;
 var secondsRemaining = 60;
 var intervalId;
@@ -16,10 +17,7 @@ function decrement() {
     if(minutesRemaining === 0 && secondsRemaining === 0) {
         stop();
         alert("Time's Up!!!");
-        $(".questions").hide();
-        $("#correct-answers").text("Correct Answers: " + correctAnswers);
-        $("#incorrect-answers").text("Incorrect Answers: " + incorrectAnswers);
-        $("#unanswered").text("Unanswered: " + unanswered);
+        gameOver();
     };
     if(secondsRemaining === 0) {
         minutesRemaining--;
@@ -38,15 +36,28 @@ function stop() {
     secondsRemaining = 60;
 };
 
-
+//functions to check if radio buttons are checked 
+//tried to add in radio buttons dynamically (in appdraft.js) but that wasn't working
+//had to add questions/answers/radio buttons to html
 function check1() {
     var question1 = document.getElementsByName("year");
     var length = question1.length
     for(i=0;i<length;i++) {
-        if(question[i].checked) {
-    
-        }
+        if(question1[i].checked) {
+            var checked1 = question1[i].value;
+            if(checked1 === "1994") {
+                correctAnswers++; //if you click on the radio button a few times it keeps adding onto correct answers, not supposed to do that
+            }
+            //unanswered questions not working, it just counts it as incorrect
+            // }else if(!checked1) {
+            //     unanswered++;
+            // }
+            else {
+                incorrectAnswers++;
+            };   
+        };
     };
+
 };
 function check2() {
     var question2 = document.getElementsByName("occupation");
@@ -54,7 +65,12 @@ function check2() {
 
     for(i=0;i<length;i++) {
         if(question2[i].checked) {
-            var checked2 = question2[i].value;  
+            var checked2 = question2[i].value; 
+            if(checked2 === "paleontologist") {
+                correctAnswers++;
+            } else {
+                incorrectAnswers++;
+            }; 
         }
     }
 };
@@ -65,6 +81,11 @@ function check3() {
     for(i=0;i<length;i++) {
         if(question3[i].checked) {
             var checked3 = question3[i].value;  
+            if(checked3 === "7") {
+                correctAnswers++;
+            } else{
+                incorrectAnswers++;
+            };
         }
     }
 };
@@ -75,6 +96,11 @@ function check4() {
     for(i=0;i<length;i++) {
         if(question4[i].checked) {
             var checked4 = question4[i].value;  
+            if(checked4 === "erica") {
+                correctAnswers++;
+            } else{
+                incorrectAnswers++;
+            };
         }
     }
 };
@@ -85,47 +111,42 @@ function check5() {
     for(i=0;i<length;i++) {
         if(question5[i].checked) {
             var checked5 = question5[i].value;  
+            if(checked5 === "rachel and joey") {
+                correctAnswers++;
+            } else{
+                incorrectAnswers++;
+            };
         }
     }
 };
+function gameOver() {
+    $(".questions").hide();
+    $("#time-left").hide();
+    $("#correct-answers").text("Correct Answers: " + correctAnswers);
+    $("#incorrect-answers").text("Incorrect Answers: " + incorrectAnswers);
+    $("#unanswered").text("Unanswered: " + unanswered);
+    $('#reset').show();
+};
+//reset does not work, the radio buttons are still selected from the first game. 
+// function reset(){
+//     $("#correct-answers").hide();
+//     $("#incorrect-answers").hide();
+//     $("#unanswered").hide();
+//     $("#start").show();
+//     $('#time-left').html('<h1>02:00</h1>');
+//     start();
+//     gameOver();
+
+// }
 
 $(document).ready(function() {
     $(".questions").hide();
     $('#time-left').html('<h1>02:00</h1>');
-    $("#start").on("click", function() {
+    $('#reset').hide()
+    $("#start").on("click", function start() {
         $("#start").hide();
         $(".questions").show();
-        run();
-        if(checked1 === "1994") {
-            correctAnswers++;
-        } else{
-            incorrectAnswers++;
-        };
-        if(checked2 === "paleontologist") {
-            correctAnswers++;
-        } else{
-            incorrectAnswers++;
-        };
-        if(checked3 === "7") {
-            correctAnswers++;
-        } else{
-            incorrectAnswers++;
-        };
-        if(checked4 === "erica") {
-            correctAnswers++;
-        } else{
-            incorrectAnswers++;
-        };
-        if(checked5 === "rachel and joey") {
-            correctAnswers++;
-        } else{
-            incorrectAnswers++;
-        };
-        
-        
-
-
-        
+        run(); 
     });
 });
 
